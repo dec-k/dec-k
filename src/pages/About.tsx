6ca16d2dbf456fdb413f865905e50d4441500f93
Declan Keighley <dec.keighley@gmail.com>
2023-11-09 20:54:28 +1300

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -6,7 +6,7 @@ export const About = () => {
       component="ul"
       sx={{ display: "flex", gap: 2, flexWrap: "wrap", p: 0, m: 0 }}
     >
-      <Card component="li" sx={{ flexGrow: 1 }}>
+      <Card component="li" sx={{ minWidth: 300, flexGrow: 1 }}>
         <CardCover>
           <img
             src="https://images.unsplash.com/photo-1557683316-973673baf926?q=80&w=2029&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -24,7 +24,7 @@ export const About = () => {
           </Typography>
         </CardContent>
       </Card>
-      <Card component="li" sx={{ flexGrow: 1 }}>
+      <Card component="li" sx={{ minWidth: 300, flexGrow: 1 }}>
         <CardCover>
           <img
             src="https://images.unsplash.com/photo-1617957718614-8c23f060c2d0?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
