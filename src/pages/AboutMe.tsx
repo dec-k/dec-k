fe39bdffd31655665660b61dc38a24a7fa862bce
Declan Keighley <dec.keighley@gmail.com>
2023-11-10 13:30:32 +1300

    Update AboutMe.tsx

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -8,6 +8,7 @@ import {
 } from "@mui/joy";
 import { useStarCardStore } from "../stores/stores";
 
+// todo: abstract into a starcard hero page, as youll probably want a couple similarly structured pages.
 export const AboutMe = () => {
   const activeStarCard = useStarCardStore((state) => state.activeStarCard);
 
@@ -22,16 +23,24 @@ export const AboutMe = () => {
         <Typography level="h3">{activeStarCard?.title}</Typography>
         <Typography level="body-sm">{activeStarCard?.subtitle}</Typography>
       </CardContent>
-      <CardOverflow variant="soft" sx={{ bgcolor: "background.level1" }}>
+      <CardOverflow>
         <Divider inset="context" />
         <CardContent orientation="horizontal">
-          <Divider orientation="horizontal" />
+          {/* Todo: this needs to be moved to a datastore/constants file. not reusable. */}
           <Typography
             level="body-md"
             fontWeight="md"
             textColor="text.secondary"
           >
-            1 hour ago
+            Hi! Thanks for visiting. I'm an experienced software developer
+            focused on Web solutions for businesses.
+            <br />
+            <br />I believe in developing practical, empathy-driven solutions
+            that puts User Experience first.
+            <br />
+            <br />
+            My favourite technologies to work with are <b>TypeScript</b>,{" "}
+            <b>Next.js</b>, <b>Vite + React</b>, and <b>C#</b>.
           </Typography>
         </CardContent>
       </CardOverflow>
