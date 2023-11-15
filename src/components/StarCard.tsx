32e7a1e8e56e7622d18b8c628bc056bd2ac7241a
Declan Keighley <dec.keighley@gmail.com>
2023-11-15 21:26:21 +1300

    slightly raised emphasis on card shadow

--- src/components/StarCard.tsx
+++ src/components/StarCard.tsx
@@ -8,7 +8,8 @@ const cardTint: string =
   "linear-gradient(to top, rgba(0,0,0,0.4), rgba(0,0,0,0) 200px), linear-gradient(to top, rgba(0,0,0,0.1), rgba(0,0,0,0) 300px)";
 
 const hoverGrowShadow = {
-  boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
+  boxShadow:
+    "rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px",
   transform: "scale(1.01)",
 };
 
