93330276ce03fb56753bdee8b5fdb0dad738be91
Declan Keighley <dec.keighley@gmail.com>
2023-11-18 20:06:11 +1300

    Update stores.ts

--- src/stores/stores.ts
+++ src/stores/stores.ts
@@ -1,4 +1,6 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
+import { StarCardModel } from "../components/StarCard";
 
 // Filtering
 export interface FilterState {
@@ -11,3 +13,21 @@ export const useFilterStore = create<FilterState>((set) => ({
 
   setFilter: (f) => set(() => ({ filter: f })),
 }));
+
+// Firebase-related data.
+export interface FirebaseCollectionState {
+  starCardData: StarCardModel[];
+  setStarCardData: (d: StarCardModel[]) => void;
+}
+
+// This syntax is trippy. See https://github.com/pmndrs/zustand/blob/main/docs/guides/typescript.md for help.
+// Basically, the ()(...) syntax is for working around a quirk in typescript where generics must be redefined.
+const useFirebaseStore = create<FirebaseCollectionState>()(
+  persist(
+    (set) => ({
+      starCardData: [],
+      setStarCardData: (d) => set(() => ({ starCardData: d })),
+    }),
+    { name: "firebaseStore" }
+  )
+);
