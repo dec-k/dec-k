bffbd65011f2a78dfbad63061c097f14cb03b155
Declan Keighley <dec.keighley@gmail.com>
2023-11-19 18:15:17 +1300

    enforced cast type returns on api calls

--- src/api/content.ts
+++ src/api/content.ts
@@ -5,14 +5,15 @@ import {
   getDoc,
   getDocs,
 } from "firebase/firestore";
+import { StarPost } from "../components/StarCard";
 
 // Gets all star posts indiscriminately.
-export async function getStarPosts(db: Firestore) {
+export async function getStarPosts(db: Firestore): Promise<StarPost[]> {
   const starPosts = collection(db, "starPosts");
   const starPostSnapshot = await getDocs(starPosts);
   const starPostList = starPostSnapshot.docs.map((doc) => doc.data());
 
-  return starPostList;
+  return starPostList as StarPost[];
 }
 
 // Gets a singular star post by its document id.
